4dc69eb2ed74dcd77a8567509e71b56dae30a07b
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-18 22:47:12 +0200

    Betty style fixes, new description, variable name changed to match bettys standards

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * Entry point - main
+ * main - Entry point
  *
- * Description : prints all single numbers of base 10
- *
- * Return : 0 (success)
- */ 
+ * Description: Prints all single-digit numbers of base 10
+ * starting from 0 followed by a new line.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int number;
+	int num;
 
-		for (number = 0; number < 10; number++)
-		{
-			putchar(number + '0');
-		}
-			putchar('\n');
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
 
 	return (0);
 }
