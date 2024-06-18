fc13ac4b978e2324f50086ac8fbe33a6158106b2
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-18 14:18:07 +0200

    Betty coding style fix

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,9 +4,10 @@
 /**
  * main - Entry point
  *
- * Description This program assigns a random number to the variable n and
- * prints the lastdigit of the number stored in the variable last_digit
- * followed by a specific message based on the value of the last digit.
+ * Description This program assigns a random number to the variable n
+ * and prints the lastdigit of the number stored in the variable
+ * last_digit followed by a specific message based on the value
+ * of the last digit.
  *
  * Return: Always 0 (Success)
  */
