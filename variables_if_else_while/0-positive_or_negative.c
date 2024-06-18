9d75a249b7a0636228a082c88a71d2db7de31c35
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-18 11:24:22 +0200

    betty fix

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: This program assigns a random number to the variable n and
+ * prints whether the number stored in the variable n is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
         int n;
