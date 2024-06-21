6dc9feccee2a3b76ec24e08e80c72427db85cc27
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-21 11:13:05 +0200

    6-abs.c betty fixed

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,18 +2,17 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: the integer to compute the absolute value of
- * 
+ * @n: the integer to compute the absolute value of 
  * Return: the absolute value of the integer
  */
 int _abs(int n)
 {
 	if (n < 0)
 	{
-		return -n;
+		return (-n);
 	}
 	else
 	{
-	return n;
+	return (n);
 	}
 }
