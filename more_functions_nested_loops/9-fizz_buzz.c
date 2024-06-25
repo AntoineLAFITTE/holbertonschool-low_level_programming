7f9b0d01085cf10d082b3a72ec8629f9c6df1ae1
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-25 14:04:27 +0200

    Betty coding style fixes

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,21 @@ int main(void)
 {
 	int i;
 
-	for(i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-		       	printf(" FizzBuzz");
+			printf(" FizzBuzz");
 		}
-		else if(i % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if(i % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf(" Buzz");
 		}
-		else if(i == 1)
+		else if (i == 1)
 		{
 			printf("%d", i);
 		}
@@ -32,7 +32,7 @@ int main(void)
 		{
 			printf(" %d", i);
 		}
-	}	
+	}
 		printf("\n");
 
 	return (0);
