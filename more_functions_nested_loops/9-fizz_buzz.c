456d143dfa10da8f7273f54c188bf72c8ad16a93
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-25 14:08:05 +0200

    Betty comments style fixes

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * fizz_buzz - prints the numbers from 1 to 100, followed by a new line
+ * main - prints the numbers from 1 to 100, followed by a new line
  * But for multiples of three print 'Fizz' instead of the number and
  * for the multiple of five print 'Buzz'.
  * For numbers which are multiples of both three and five print 'FizzBuzz'.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
