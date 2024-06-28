bec39b3c39b1a7c24d2e3543daeb060f0d582c1b
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-28 13:29:23 +0200

    100-atoi.c done

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi -  converts a string to an integer.
+ * @s: pointer to the string to be converted
+ *
+ * Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int found_digit = 0;
+
+	while (*s)
+	{
+		if (*s =='_')
+		{
+			sign *= -1;
+		}
+		else if (*s == '+')
+		{
+
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			found_digit = 1;
+			result = result * 10 + (*s - '0');
+		}
+		else if (found_digit)
+		{
+			break;
+		}
+		s++;
+	}
+
+	return (sign * result);
+}
