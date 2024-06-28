a3f9f2b57fb1eda985888161b1a9dd7a82d9b41d
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-28 15:35:39 +0200

    0-strcat.c done

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes to use from src
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int dest_length = 0;
+
+	while (dest[dest_length] != '\0')
+	{
+		dest_lenght++;
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_length + i] = src[i];
+	}
+
+	dest[dest_length + i] = src[i];
+	}
+
+	dest[dest_length + i] = '\0';
+
+	return dest;
+}
