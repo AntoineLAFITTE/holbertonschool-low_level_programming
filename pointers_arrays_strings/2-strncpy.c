1e3cf81db938b5e29b7a64567b5c1b0fe7e610c8
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-06-30 13:51:22 +0200

    betty coding style fixes

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -6,7 +6,8 @@
  * @src: source string
  * @n: number of bytes to copy
  *
- * This function copies up to n bytes from the string pointed to by src to dest.
+ * This function copies up to n bytes from the string pointed
+ * to by src to dest.
  * If the length of src is less than n, the remainder of dest will be padded
  * with null bytes. If the length of src is greater than or equal to n, the
  * string pointed to by dest will not be null-terminated.
@@ -27,5 +28,5 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = '\0';
 	}
 
-	return dest;
+	return (dest);
 }
