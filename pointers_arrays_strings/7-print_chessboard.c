36d3f4daace42486cc710bae53fb68c3584bbd19
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-02 14:18:11 +0200

    7-print_chessboard.c done

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Prints the chessboard using an array of characters.
+ * @a: Pointer to the 2D array of characters wich is used as a chest board.
+ * The function iterats through each row of the aray(chestboard) and prtins. 
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}	
+}
