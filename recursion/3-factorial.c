3e9ded996155d12b52e5a3804e2e9ccb52d055ef
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-03 15:38:49 +0200

    3-factorial.c betty fixes

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -4,12 +4,12 @@
  * factorial - Returns the factorial of a given number.
  * @n: the number to return the factorial of.
  * Return : the factorial of the number. If n is less than 0,
- * 	returns -1 to indicate an error.
+ * returns -1 to indicate an error.
  */
 int factorial(int n)
 {
 	if (n < 0)
-	{	
+	{
 		return (-1);
 	}
 	if (n == 0)
