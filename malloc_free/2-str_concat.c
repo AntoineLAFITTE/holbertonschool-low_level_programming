83526cb4e6084f17ebcab2ea3de82074a77ca214
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-09 17:01:24 +0200

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,49 +10,41 @@
  * Return: A Pointer to new alloacted space in memory where the result of
  * the two concatenated string is. NULL if it fails.
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
-	int len1 = 0;
-	int len2 = 0;
-	int total_len;
-	int i = 0;
-	
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
+	int len1, len2, total_len, i;
+	char *concatenated_str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = 0;
 	while (*(s1 + len1) != '\0')
 	{
 		len1++;
 	}
 
-	while (*s2 + len2) != '\0')
+	len2 = 0;
+	while (*(s2 + len2) != '\0')
 	{
 		len2++;
 	}
 
 	total_len = len1 + len2;
 
-	concat_str = (char *) malloc((total_len * sizeof(char) +1);
+	concatenated_str = (char *) malloc(total_len * sizeof(char) + 1);
 
-	if (concat_str == NULL)
-
-		return (NULL);}
+	if (concatenated_str == NULL)
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
-
-		concat_str[i] = s1[i];
+		str[i] = s1[i];
 
 	for (i = 0; i < len2; i++)
+		concatenated_str[i + len1] = s2[i];
 
-		concat_str[i + len1] = s2[i];
-
-	return (concat_str);
-
+	return (cocatenated_str);
 }
