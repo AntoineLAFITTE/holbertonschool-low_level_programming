17dd53d392329bb2cad7d661b27cbaceaa8e9209
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-10 10:59:15 +0200

    1-string_nconcat.c fixes

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 + "";
+		s2 = "";
 
 	while (s1[len1] != '\0')
 		len1++;
@@ -28,12 +28,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= len2)
 		n = len2;
-	concat = malloc(len1 + n +1);
-	if (concat == NULL);
+
+	concat = malloc(len1 + n + 1);
+	if (concat == NULL)
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
 	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
 
 	concat[i + j] = '\0';
 
