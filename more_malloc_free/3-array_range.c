de847f8c0b27f2725779f463cde2afb06569c93b
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-10 14:59:39 +0200

    3-array_range.c betty fixes

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,6 +14,7 @@ int *array_range(int min, int max)
 	int i;
 	int size;
 	if (min > max)
+
 		return (NULL);
 
 	size = max - min + 1;
@@ -25,5 +26,5 @@ int *array_range(int min, int max)
 	for (i = 0; i < size; i++)
 		array[i] = min + i;
 
-	return(array);
+	return (array);
 }
