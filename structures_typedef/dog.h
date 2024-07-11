040b515b51e88656830fe46ce7cc9909bffd9b6d
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-11 17:01:51 +0200

    dog.h done

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOH_H
+/**
+ * struct dog - Stucture for storage of dog's informations.
+ * @name: Name of the dog (char pointer)
+ * @age: Age of the dog (float)
+ * @owner: Name of the dog owner (char pointer)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
