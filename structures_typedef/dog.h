f59c072431633a6ea2112dbccbe49626f83d8c35
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-11 17:58:43 +0200

    dog_t typedef added for the type struct dog added in dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,7 +15,10 @@ struct dog
 	char *owner;
 };
 
+/* new type definition */
+typedef struct dog dog_t;
 
+/* Prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
