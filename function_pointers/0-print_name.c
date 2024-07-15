98e2d00708eb53f81ac3d3805a0e38037b98af13
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-15 13:19:27 +0200

    0-print_name.c done

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using the function pointed by f
+ * @name: pointer to the name
+ * @f: pointer to the function used to print the name
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	else
+		f(name);
+}
