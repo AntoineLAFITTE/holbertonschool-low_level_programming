91a2f3741feeea81669b59d1b720e1a05c26fe88
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-17 11:36:24 +0200

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,8 +12,8 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	if(n == 0)
-	return(0);
+	if (n == 0)
+	return (0);
 
 	va_start(args, n);
 
@@ -24,5 +24,5 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_end(args);
 
-	return(sum);
+	return (sum);
 }
