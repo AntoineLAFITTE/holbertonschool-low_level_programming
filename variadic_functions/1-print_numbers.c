26bcb71ebc17f6bc5590258413014ebeb4f11252
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-17 11:53:46 +0200

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/*
+ * print_numbers - Prints numbers, Followed by a new line.
+ *
+ * @separator: string to be printed between values.
+ * @n: number of integers passed to the function.
+ *
+ * Return: Numbers passed as arguments to the function.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
