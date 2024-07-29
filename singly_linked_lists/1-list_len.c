af0d20391d134f88e41940f621a876fe1ddfe1dd
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-29 11:52:24 +0200

    1-list_len.c betty style fix

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,13 +8,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t counter = 0;
+	size_t counter = 0;
 
-    while (h)
-    {
+	while (h)
+	{
 		counter++;
 		h = h->next;
-    }
+	}
 
 	return (counter);
 }
