0082987a24abdbd3ecc8085a454ec9bc07a0d8a7
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-31 15:32:37 +0200

    Compiled

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@
 * at the begining of a doubly linked list.
 * Return: the address of new element, or NULL if it failed.
 */
-dlistint_t *add_dnodeint(dlistint **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint(dlistint **head, const int n)
 
 	new_node->n = n;
 	new_node-> prev = NULL;
-	nrw_node->next = *head;
+	new_node->next = *head;
 
 	if (*head != NULL)
 		(*head)->prev = new_node;
