2115b3969ad047ff9e034df26dbbd3e449cd875d
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-31 15:40:01 +0200

    2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,10 +12,10 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    	dlistint_t *new_node;
+	dlistint_t *new_node;
 
-    	new_node = malloc(sizeof(dlistint_t));
-    	if (new_node == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
@@ -29,4 +29,3 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	return (new_node);
 }
-
