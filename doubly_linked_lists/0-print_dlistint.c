96ec7d5f0b09edd12125f95624bbdec52882a7d2
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-31 12:00:11 +0200

    0-print_dlistint.c kernel documentation style fix

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -5,6 +5,9 @@
  * print_dlisrint - prints all the elements of a dlistint_t list.
  * @h: pointer to the head of the doubly linked list.
  *
+ * Description: This function traverses a doubly linked list,
+ * prints each element's integer value and counting the number of nodes
+ * in the list.
  * Return: the number of nodes.
  */
 size_t print_dlistint(const dlistint_t *h)
