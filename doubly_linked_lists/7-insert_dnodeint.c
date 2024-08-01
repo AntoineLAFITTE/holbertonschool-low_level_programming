eabe1e5a1c53a9deb7a719e5a70ae5bc6491b040
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-08-01 11:53:19 +0200

    remake

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,37 +14,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp;
-	unsigned int current_index = 0;
+	dlistint_t *temp = *h, *new_node;
 
-		if (h == NULL)
-		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	temp = *h;
-	while (temp != NULL && current_index < idx - 1)
-	{	
-		temp = temp->next;
-		current_index++;
-	}
-
-	if (temp == NULL || (temp->next == NULL && current_index + 1 != idx))
-	{
-		free(new_node);
-		return (NULL);
-	}
-	if (temp->next !=NULL)
-		temp->next->prev = new_node;
-
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	temp->next->prev = new_node;
 	temp->next = new_node;
-	
+
 	return (new_node);
 }
