62797167ddeea11bd3589fef25bed0b98da1f44a
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-08-04 12:29:21 +0200

    1-create_file.c rename file_descriptor in file_desc for betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,36 +3,35 @@
 /**
  * create_file - Creates a file and writes text content to it.
  * @filename: Pointer to the name of the file to create.
- * @text_content: Pointer to a NULL terminated string to write to the file.
- * 
+ * @text_content: Pointer to a NULL terminated string to write to the file. 
  * Return: 1 on success, -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
+	int file_desc;
 	ssize_t bytes_written;
 	size_t length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_descriptor == -1)
+	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_desc == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
 		while (text_content[length])
 			length++;
-		bytes_written = write(file_descriptor, text_content, length);
+		bytes_written = write(file_desc, text_content, length);
 		if (bytes_written == -1 || (size_t)bytes_written != length)
 		{
-			close(file_descriptor);
+			close(file_desc);
 			return (-1);
 		}
 	}
 
-	close(file_descriptor);
+	close(file_desc);
 	return (1);
 }
 
