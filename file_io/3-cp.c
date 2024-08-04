e133ba502c12b1cc90640b830c8d4b0706514f0b
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-08-04 14:41:01 +0200

    3-cp.c more fixes

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
